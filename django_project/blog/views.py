373da5741c9a92487f397f63f734cb56b18fe919
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-08-05 10:39:21 +0200

    adds a basic about page to blog

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<H1>Blog Home</H1>')
-    
+
+def about(request):
+    return HttpResponse('<H1>Blog About</H1>')
+
