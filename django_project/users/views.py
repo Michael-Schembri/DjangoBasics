75127420764ef4e19b188bff9c981e2a6569886b
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-08-08 11:50:34 +0200

    Adds basic login/logout functionality

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -7,9 +7,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request,f'account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request,f'Your account has been created!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form})
