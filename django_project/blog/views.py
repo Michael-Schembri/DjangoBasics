96dd462a84405726452dadb5e1ed34ee256d2ebe
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-08-12 11:11:50 +0200

    creates new post view

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView
+)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class PostListView(ListView):
@@ -10,7 +15,15 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
-    model = Post 
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post  
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):    
     return render(request, 'blog/about.html', {'title':"About"})
