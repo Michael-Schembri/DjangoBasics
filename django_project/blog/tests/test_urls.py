fa3dd6ff7aa18cafbba49870bb610f3fb122bc02
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-08-12 10:38:41 +0200

    Adds post detail view, links and tests

--- django_project/blog/tests/test_urls.py
+++ django_project/blog/tests/test_urls.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from blog.views import about, PostListView
+from blog.views import about, PostListView, PostDetailView
 
 class Test_Urls(TestCase):
     def test_home_url_resolves(self):
         url = reverse('blog-home')
         assert resolve(url).func.view_class == PostListView
+
+    def test_post_detail_url_resolves(self):
+        url = reverse('post-detail',args=[1])
+        assert resolve(url).func.view_class == PostDetailView
 
     def test_about_url_resolves(self):
         url = reverse('blog-about')
