4d84286538a06a75541d3fdfe934bfff0ec669c8
Michael-Schembri <michael_schembri@hotmail.co.uk>
2020-08-12 11:39:39 +0200

    adds delete view

--- django_project/blog/tests/test_urls.py
+++ django_project/blog/tests/test_urls.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from blog.views import about, PostListView, PostDetailView, PostCreateView, PostUpdateView
+from blog.views import (
+    about, 
+    PostListView, 
+    PostDetailView, 
+    PostCreateView, 
+    PostUpdateView,
+    PostDeleteView
+)
 
 class Test_Urls(TestCase):
     def test_home_url_resolves(self):
@@ -15,9 +22,14 @@ class Test_Urls(TestCase):
         url = reverse('post-create')
         assert resolve(url).func.view_class == PostCreateView
 
-    def test_post_create_url_resolves(self):
-        url = reverse('post-create', args=[1])
-        assert resolve(url).func.view_class == PostCreateView
+    def test_post_update_url_resolves(self):
+        url = reverse('post-update', args=[1])
+        assert resolve(url).func.view_class == PostUpdateView
+
+    def test_post_delete_url_resolves(self):
+        url = reverse('post-delete', args=[1])
+        assert resolve(url).func.view_class == PostDeleteView
+    
 
     def test_about_url_resolves(self):
         url = reverse('blog-about')
--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,12 +5,14 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
+    PostDeleteView,
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
